4140d31bcd06b3c0f22aea71b9a2c050586476f8
midassilver <emanuelstar400@gmail.com>
2026-05-10 22:09:48 -0600

    Correccion de errores menores

--- Proyecto/Secuencias_ADN/src/main.c
+++ Proyecto/Secuencias_ADN/src/main.c
@@ -1,8 +1,9 @@
+
 #include "../include/cadenas.h"
 #include "../include/busqueda_MPI.h"
 #include "../include/busqueda_lineal.h"
+#include "../include/cadenas_pthread.h"
 #include "../include/parametros.h"
-
 
 int main(int argc, char* argv[]) {
 
@@ -10,31 +11,28 @@ int main(int argc, char* argv[]) {
 
     procesar_argumentos(argc, argv, &parametros);
 
-    char *cadena_adn = reservar_vector(parametros.longitud_adn);
+    char *cadena_adn =reservar_vector(parametros.longitud_adn);
 
-    patron_t *patrones =reservar_patrones(parametros.cantidad_patrones, parametros.longitud_patron);
+    patron_t *patrones = reservar_patrones(parametros.cantidad_patrones, parametros.longitud_patron);
 
-    generar_adn(cadena_adn, parametros.longitud_adn);
+    generar_adn(cadena_adn,parametros.longitud_adn);
 
-    generar_patrones(patrones, parametros.longitud_patron, parametros.cantidad_patrones);
+    generar_patrones( patrones, parametros.longitud_patron,parametros.cantidad_patrones);
 
-    buscar_patrones_lineal(cadena_adn, parametros.longitud_adn, patrones, parametros.cantidad_patrones);
+    /*
+        BUSQUEDA LINEAL
+    */
+    printf("\n=== BUSQUEDA LINEAL ===\n");
+
+    buscar_patrones_lineal(cadena_adn,parametros.longitud_adn, patrones,parametros.cantidad_patrones);
 
     for (int i = 0; i < parametros.cantidad_patrones; i++) {
 
-        printf(
-            "Patron %d [%s] - Estado: [%d]",
-            i,
-            patrones[i].patron,
-            patrones[i].estado
-        );
+        printf("Patron %d [%s] - Estado [%d]", i, patrones[i].patron, patrones[i].estado);
 
         if (patrones[i].estado == COINCIDENCIA) {
 
-            printf(
-                " - Posicion encontrada: %d\n",
-                patrones[i].encontrado_en
-            );
+            printf( " - Posicion %d\n", patrones[i].encontrado_en);
 
         } else {
 
@@ -42,40 +40,60 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    /*
+        BUSQUEDA PTHREAD
+    */
+    printf("\n=== BUSQUEDA PTHREAD ===\n");
+
+    buscar_patrones_pthread(cadena_adn, parametros.longitud_adn,patrones, parametros.cantidad_patrones,parametros.numero_hilos);
+
     for (int i = 0; i < parametros.cantidad_patrones; i++) {
+
+        printf("Patron %d [%s] - Estado [%d]", i, patrones[i].patron, patrones[i].estado );
+
+        if (patrones[i].estado == COINCIDENCIA) {
+
+            printf(" - Posicion %d\n", patrones[i].encontrado_en );
+
+        } else {
+
+            printf(" - No encontrado\n");
+        }
+    }
+
+    /*
+        BUSQUEDA MPI
+    */
+    printf("\n=== BUSQUEDA MPI ===\n");
+
+    patron_t *patrones_MPI = buscar_patrones_MPI(parametros);
+
+    for (int i = 0; i < parametros.cantidad_patrones; i++) {
+
+        printf("Patron %d [%s] - Estado [%d]",
+            i, patrones_MPI[i].patron, patrones_MPI[i].estado);
+
+        if (patrones_MPI[i].estado == COINCIDENCIA) {
+
+            printf(" - Posicion %d\n",patrones_MPI[i].encontrado_en);
+
+        } else {
+
+            printf(" - No encontrado\n");
+        }
+    }
+
+    /*
+        Liberar memoria
+    */
+    for (int i = 0; i < parametros.cantidad_patrones; i++) {
+
         free(patrones[i].patron);
     }
 
     free(patrones);
+
     free(cadena_adn);
-
-    /* Empezando aca va el codigo encargado de MPI*/
-    //Busqueda con MPI
-    patron_t *patrones_MPI = buscar_patrones_MPI(parametros);
-
-    //Print
-        printf("Patrones_MPI con MPI\n");
-        for (int i = 0; i < parametros.cantidad_patrones; i++) {
-
-            printf(
-                "Patron %d [%s] - Estado: [%d]",
-                i,
-                patrones_MPI[i].patron,
-                patrones_MPI[i].estado
-            );
-            
-            if (patrones_MPI[i].estado == COINCIDENCIA) {
-                
-                printf(
-                    " - Posicion encontrada: %d\n",
-                    patrones_MPI[i].encontrado_en
-                );
-                
-            } else {
-                
-                printf(" - No encontrado\n");
-            }
-        }
 
     return 0;
 }
