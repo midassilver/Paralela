42ebf5bb15b8d0e6afa99a57ee8a059070badf62
midassilver <emanuelstar400@gmail.com>
2026-05-10 21:40:13 -0600

    Se agrego la busqueda con hilos y se elimino un codigo priliminar

---
+++ Proyecto/Secuencias_ADN/include/busqueda_pthread.h
@@ -0,0 +1,18 @@
+
+#ifndef BUSQUEDA_PTHREAD_H
+#define BUSQUEDA_PTHREAD_H
+
+#include <pthread.h>
+#include "cadenas.h"
+
+
+#define NO_ENCONTRADO -1
+#define ENCONTRADO 1
+
+
+void* trabajador_hilo(void* arg);
+
+
+void buscar_patrones_pthread(const char* cadena_adn,int longitud_adn,patron_t* patrones,int cantidad_patrones,int cantidad_hilos);
+
+#endif
